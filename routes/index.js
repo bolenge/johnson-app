d0e74717a1b250426e1e53c1f23bf41a0b6fd0f5
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2024-10-11 08:30:27 +0100

    add page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'GS Mont-Amba' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'A propos' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+router.get('/courses', function(req, res, next) {
+  res.render('courses', { title: 'Cours' });
+});
+
+router.get('/team', function(req, res, next) {
+  res.render('team', { title: 'Professeurs' });
 });
 
 module.exports = router;
